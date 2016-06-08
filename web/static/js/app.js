b0e483532c82e3767e44c1c0e907f809b2013a4e
msp <matt@cenatus.org>
2016-06-08 20:43:30 +0100

    A visitor can view the latest image and navigate images via the thumnbails

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -20,71 +20,93 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-var IMAGES = [
-  {
-    "taken_at": "2015-09-23T03:23:37Z",
-    "s3_url": "http://cyanometer.s3.aws.com/12345",
-    "blueness_index": "4"
-  },
-  {
-    "taken_at": "2016-06-05T16:04:17Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-05.06.2016-16_04_16-large.jpg",
-    "blueness_index": "4"
-  },
-  {
-    "taken_at": "2016-06-07T08:42:58Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-07.06.2016-08_42_57-large.jpg",
-    "blueness_index": "51"
-  },
-  {
-    "taken_at": "2016-06-07T08:50:39Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-07.06.2016-08_50_39-large.jpg",
-    "blueness_index": "51"
-  },
-  {
-    "taken_at": "2016-06-07T08:57:59Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-07.06.2016-08_57_59-large.jpg",
-    "blueness_index": "51"
-  },
-  {
-    "taken_at": "2016-06-07T09:03:26Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-07.06.2016-09_03_26-large.jpg",
-    "blueness_index": "51"
-  },
-  {
-    "taken_at": "2016-06-07T07:25:38Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-07.06.2016-09_25_38-large.jpg",
-    "blueness_index": "50"
-  },
-  {
-    "taken_at": "2016-06-07T07:25:38Z",
-    "s3_url": "https://s3.eu-central-1.amazonaws.com/cyanometer/sky-07.06.2016-09_25_39-large.jpg",
-    "blueness_index": "50"
-  }
-]
-
-
 import React from "react"
 import ReactDOM from "react-dom"
 
 class CyanDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: '',
+      images: ''
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  componentDidMount() {
+    var self = this;
+
+    $.ajax({
+      url: this.props.source,
+      dataType: 'json',
+      success: (data) => {
+        // TODO Fix initial image when we have real data/images!
+        var firstImage = 'http://placehold.it/1200x675';
+        if (data[5]) {
+          firstImage = data[5].s3_url;
+        }
+
+        self.setState({data: data, image: firstImage});
+      },
+      error: (xhr, status, err) => {
+        console.error(self.props.source, status, err.toString());
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
+  handleUserInput(image) {
+    this.setState({
+      image: image
+    });
+  }
+
   render() {
+    console.log('CyanDisplay.render');
+
+    var divStyle = {
+      color: 'black',
+      backgroundImage: 'url('+this.state.image+')'
+    };
+
     return (
       <div>
-        <img src="http://placehold.it/1200x800" />
-        <CyanThumbnails images={this.props.images} />
+        <div style={divStyle} className="cyan-display-main">
+          <h4>{this.state.image}</h4>
+        </div>
+        <CyanThumbnails images={this.state.data} onUserInput={this.handleUserInput} />
       </div>
     );
   }
 }
 
 class CyanThumbnails extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(image) {
+    console.log('CyanThumbnails.handleUserInput, image: '+image);
+    this.props.onUserInput(
+      image
+    );
+  }
+
   render() {
+    console.log('CyanThumbnails.render, this.props.images: '+this.props.images);
     var rows = [];
     var lastCategory = null;
-    this.props.images.forEach(function(image) {
-      rows.push(<CyanThumbnail image={image} key={image.s3_url} />);
-    });
+    if (this.props.images) {
+      this.props.images.forEach(function(image) {
+        rows.push(<CyanThumbnail image={image} key={image.s3_url} onUserInput={this.handleUserInput} />);
+      }.bind(this));
+    }
     return (
       <div className='grid' id="thumbnails">
         {rows}
@@ -94,14 +116,27 @@ class CyanThumbnails extends React.Component {
 }
 
 class CyanThumbnail extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.onUserInput(
+      this.props.image.s3_url
+    );
+  }
+
   render() {
+    console.log('CyanThumbnail.render');
     var divStyle = {
       color: 'white',
       backgroundImage: 'url(' + 'http://placehold.it/150x150' + ')'
     };
 
     return (
-      <div className="thumbnail" style={divStyle}>
+      <div className="thumbnail" style={divStyle} onClick={this.handleClick}>
         <p>{this.props.image.blueness_index}</p>
       </div>
     );
@@ -109,7 +144,7 @@ class CyanThumbnail extends React.Component {
 }
 
 ReactDOM.render(
-  <CyanDisplay images={IMAGES}/>,
+  <CyanDisplay source="/api/images"/>,
   document.getElementById("cyan-display")
 )
 
