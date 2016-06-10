4603caca32193ac166075435565b95f4db5d5a9a
msp <matt@cenatus.org>
2016-06-10 20:19:51 +0100

    Scale the trigger in the pic menu

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -376,6 +376,7 @@ function toggleMenu(event) {
   if (open) {
     console.log("opening...");
     var tl = new TimelineLite();
+    tl.to(trigger, 0.6, {scale:1, transformOrigin: "50% 50%", ease: Expo.easeInOut }, 0)
   	tl.to(items, 0.2, {scale:1, ease:Back.easeOut.config(4)}, 0.05);
     for(var i=0; i<items.length; i++){
       tl.to(items[i], 0.7, {rotation:-i*angle + "deg", ease:Bounce.easeOut}, 0.35);
@@ -396,6 +397,7 @@ function closePieMenu() {
     tl.to(items[i], 0.3, {rotation: 0, ease:Circ.easeOut}, 0.05);
   }
   tl.to(items, .3, {scale:0, ease:Back.easeIn}, 0.3);
+  tl.to(trigger, 0.6, {scale:0.5, transformOrigin: "50% 50%", ease: Expo.easeInOut }, 0)
   label.innerHTML = "+";
   svg.style.pointerEvents = "none";
 
