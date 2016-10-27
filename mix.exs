243e6a2f28ea8bf4b4614599b3549ef105b59fd0
msp <matt@cenatus.org>
2016-10-27 19:40:11 +0100

    Dependency updates to unbork local compile.
    - https://github.com/phoenixframework/phoenix/issues/1768
    - https://github.com/phoenixframework/phoenix/issues/1953

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule Cyanometer.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
+    [{:phoenix, "~> 1.1.6"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+     {:phoenix_html, "~> 2.6.0"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"}]
