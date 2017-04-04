d8793617c38753ff93928a307fa5b8921792cda6
msp <matt@cenatus.org>
2017-04-04 20:03:40 +0100

    https  for images

--- web/static/js/imageUtils.js
+++ web/static/js/imageUtils.js
@@ -1,6 +1,6 @@
 export class ImageUtils {
   static cloudinaryURL(thumbnail = false) {
-    let baseURL = "http://res.cloudinary.com/mota/image/fetch/"
+    let baseURL = "https://res.cloudinary.com/mota/image/fetch/"
     if (thumbnail) {
       baseURL += "w_178,h_100,c_thumb/"
     } else {
