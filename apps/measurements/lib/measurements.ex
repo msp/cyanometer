8bebc8ed5de67b600020bdfd1139835a0c72c063
msp <matt@cenatus.org>
2017-07-12 17:18:33 +0100

    Create measurements project

---
+++ apps/measurements/lib/measurements.ex
@@ -0,0 +1,18 @@
+defmodule Measurements do
+  @moduledoc """
+  Documentation for Measurements.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Measurements.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/measurements/test/measurements_test.exs
@@ -0,0 +1,8 @@
+defmodule MeasurementsTest do
+  use ExUnit.Case
+  doctest Measurements
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
