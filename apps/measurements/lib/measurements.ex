5ff78049343501439e7ae2ddb7dfa8befd376add
msp <matt@cenatus.org>
2017-07-14 16:22:45 +0100

    Parser test

--- apps/measurements/lib/measurements.ex
+++ apps/measurements/lib/measurements.ex
@@ -26,6 +26,18 @@ defmodule Measurements do
     %{ozone: "??", particles: "??", sulphite: "??", nitrogen: "??"}
   end
 
+  def parse_ljubljana(node) do
+    ozone     = node |> xpath(~x"//postaja[@sifra='E21']/o3/text()")
+    particles = node |> xpath(~x"//postaja[@sifra='E21']/pm10/text()")
+    sulphite  = node |> xpath(~x"//postaja[@sifra='E21']/so2/text()")
+    nitrogen  = node |> xpath(~x"//postaja[@sifra='E21']/no2/text()")
+
+    %{ozone: ozone,
+      particles: particles,
+      sulphite: sulphite,
+      nitrogen: nitrogen}
+  end
+
   ############################## Server ########################################
   def handle_call(:get_ljubljana, _from, state) do
     {:reply, state[:ljubljana], state}
@@ -51,28 +63,5 @@ defmodule Measurements do
   defp fetch_ljubljana_xml(url) do
     {:ok, {_, _, body}} = :httpc.request(String.to_char_list(url))
     body
-  end
-
-  defp parse_ljubljana(node) do
-    # <postaja sifra="E21" ge_dolzina="14.512704" ge_sirina="46.065497" nadm_visina="299">
-    #  <merilno_mesto>Ljubljana Bežigrad</merilno_mesto>
-    #  <datum_od>2016-06-11 00:00</datum_od>
-    #  <datum_do>2016-06-11 01:00</datum_do>
-    #  <so2>5</so2>
-    #  <co>0.2</co>
-    #  <o3>39</o3>
-    #  <no2>32</no2>
-    #  <pm10>16</pm10>
-    # </postaja>
-
-    ozone     = node |> xpath(~x"//postaja[@sifra='E21']/o3/text()")
-    particles = node |> xpath(~x"//postaja[@sifra='E21']/pm10/text()")
-    sulphite  = node |> xpath(~x"//postaja[@sifra='E21']/so2/text()")
-    nitrogen  = node |> xpath(~x"//postaja[@sifra='E21']/no2/text()")
-
-    %{ozone: ozone,
-      particles: particles,
-      sulphite: sulphite,
-      nitrogen: nitrogen}
   end
 end
