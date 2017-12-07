b2c75d3a839efbc4c2a310d21494ff5c5b4d6120
msp <matt@cenatus.org>
2017-12-07 00:54:12 +0000

    Remove open/close animation that skews the position of the first segment from 12 o'clock

--- apps/cyanometer/web/static/vendor/pie_menu_utils.js
+++ apps/cyanometer/web/static/vendor/pie_menu_utils.js
@@ -31,9 +31,10 @@ function toggleMenu(event) {
     var tl = new TimelineLite();
     tl.to(trigger, 0.6, {scale:1, transformOrigin: "50% 50%", ease: Expo.easeInOut }, 0)
     tl.to(items, 0.2, {scale:1, ease:Back.easeOut.config(4)}, 0.05);
-    for(var i=0; i<items.length; i++){
-      tl.to(items[i], 0.7, {rotation:-i*angle + "deg", ease:Bounce.easeOut}, 0.35);
-    }
+    // MSP using this open/close animation skews the position of the first segment from 12 o'clock
+    // for(var i=0; i<items.length; i++){
+    //   tl.to(items[i], 0.7, {rotation:-i*angle + "deg", ease:Bounce.easeOut}, 0.35);
+    // }
     label.innerHTML = "-";
     svg.style.pointerEvents = "auto";
   } else {
@@ -45,9 +46,10 @@ function toggleMenu(event) {
 function closePieMenu() {
   open = false;
   var tl = new TimelineLite();
-  for(var i=0; i<items.length; i++){
-    tl.to(items[i], 0.3, {rotation: 0, ease:Circ.easeOut}, 0.05);
-  }
+  // MSP using this open/close animation skews the position of the first segment from 12 o'clock
+  // for(var i=0; i<items.length; i++){
+  //   tl.to(items[i], 0.3, {rotation: 0, ease:Circ.easeOut}, 0.05);
+  // }
   tl.to(items, .3, {scale:0, ease:Back.easeIn}, 0.3);
   tl.to(trigger, 0.6, {scale:0.5, transformOrigin: "50% 50%", ease: Expo.easeInOut }, 0)
   label.innerHTML = "+";
